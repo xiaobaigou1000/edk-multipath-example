b7db50c48be6eac3e5a29493c117a9498b487919
xiaobaigou1000 <xiaobaigou2000@outlook.com>
2025-08-16 13:54:31 +0800

    Export COM debug log

--- run_ovmf.sh
+++ run_ovmf.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -L . --bios $(pwd)/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd -hda fat:rw:hda-contents -net none
+qemu-system-x86_64 -L . --bios $(pwd)/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd -hda fat:rw:hda-contents -net none -debugcon file:debug.log -global isa-debugcon.iobase=0x402
