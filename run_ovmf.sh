4283b233545a6665a0e4271fbd6e1a7faf479733
xiaobaigou1000 <xiaobaigou2000@outlook.com>
2025-08-16 21:32:26 +0800

    Use relative path

--- run_ovmf.sh
+++ run_ovmf.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -L . --bios $(pwd)/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd -hda fat:rw:hda-contents -net none -debugcon file:debug.log -global isa-debugcon.iobase=0x402
+qemu-system-x86_64 -L . --bios $(pwd)/edk2/Build/OvmfX64/DEBUG_GCC5/FV/OVMF.fd -hda fat:rw:hda-contents -net none -debugcon file:$(pwd)/debug.log -global isa-debugcon.iobase=0x402
